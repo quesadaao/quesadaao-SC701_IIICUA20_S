3e3fcab83f976be3ac504001d643b34377d606e4
Guillermo Sotomayor B <guillesotoblanco@gmail.com>
2020-11-21 10:48:39 -0600

    Commit semana 10

---
+++ Code-P2/UI.MaestroDetalle/UI/Models/Metric.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace UI.Models
+{
+    public partial class Metric
+    {
+        public int MetricId { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ Code-P2/UI.MaestroDetalle/UI2/Data/PurchaseData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UI2.Models;
+
+namespace UI2.Data
+{
+    public class PurchaseData
+    {
+        
+
+        //public List<ProductPhoto> GetProductPhoto()
+        //{
+        //    List<ProductPhoto> resul;
+        //    try
+        //    {
+        //        resul = _context.ProductPhoto.FromSqlRaw("Exec ");
+        //    }catch(Exception ex)
+        //    {
+
+        //    }
+        //}
+
+
+    }
+}
